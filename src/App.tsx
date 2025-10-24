52eb3b53b18f2dea51cb90beb15ee7147478c9e0
POM <peter@omalley.io>
2025-10-24 22:17:38 +0200

    Fix build errors: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import { Toaster } from 'sonner'
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Toaster position="top-center" />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
 
--- src/components/PrizeSection.tsx
+++ src/components/PrizeSection.tsx
@@ -1,4 +1,4 @@
-import { Trophy, Building2, Award } from 'lucide-react'
+import { Trophy, Award } from 'lucide-react'
 
 export default function PrizeSection() {
   return (
